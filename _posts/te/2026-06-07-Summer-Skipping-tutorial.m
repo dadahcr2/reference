b5b6d4613cd856220d1f142e71cd6cbb74c86a15
dadahcr2 <73378503+dadahcr2@users.noreply.github.com>
2026-06-07 15:51:36 -0400

    Create 2026-06-07-Summer-Skipping-tutorial.m

---
+++ _posts/te/2026-06-07-Summer-Skipping-tutorial.m
@@ -0,0 +1,7 @@
+---
+link: https://youtu.be/9IGlq0TRi8I?si=RO5xUSv6-sZx6u1f&t=216
+tag: TE
+header:
+  teaser: 
+---
+<iframe width="560" height="315" src="https://www.youtube.com/embed/9IGlq0TRi8I?si=RO5xUSv6-sZx6u1f&amp;start=216" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
